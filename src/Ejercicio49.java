d39b4095ce9570594cb3a51071f1427463de38ef
Morgan <morgan.cordoba@hotmail.com>
2016-10-06 11:10:11 +0200

    Ejercicio49
    Hacer un algoritmo que escriba un triabgulo Floyd. Leeremos un numero n
    que representara el numero de filas a escribir
    Exemple por N=4
    1
    2 3
    4 5 6
    7 8 9 10

---
+++ src/Ejercicio49.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio49 {
+
+	public static void main(String[] args) {
+		/* Hacer un algoritmo que escriba un triabgulo Floyd. Leeremos un numero n que representara el numero de filas a escribir
+		Exemple per N=4
+		1
+		2 3
+		4 5 6
+		7 8 9 10*/
+
+		Scanner entrada = new Scanner (System.in);
+		
+		int linea, c = 1, x, y;
+		
+		System.out.println("¿Cuantas lineas quieres que tenga el triangulo Floyd?");
+		
+		linea = entrada.nextInt();
+		
+		for(x = 0; x != linea; x++)
+		{
+			for(y = 0; y <= x; y++)
+			{
+				System.out.print(c);
+				
+				c++;
+			}
+			System.out.println("");
+		}
+	}
+
+}
