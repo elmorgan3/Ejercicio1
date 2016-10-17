27a510c8b4edf63f422942a423308c28ee271efc
Morgan <morgan.cordoba@hotmail.com>
2016-10-17 15:34:18 +0200

    Traducido los textos y comentado

--- src/Ejercicio50.java
+++ src/Ejercicio50.java
@@ -3,19 +3,11 @@ import java.util.Scanner;
 public class Ejercicio50 {
 
 	public static void main(String[] args) {
-		/* 50 Fer un programa que calculi el N termes de la sèrie Fibonacci
-
-		Exemple per N = 7, el resultat seria, ja
-
-		a. La sèrie de Fibonacci seria: 1 1 2 3 5 8 13 ..
-	
-		b. Aquesta sèrie es calcula tenint en compte que el primer i segon valor de la sèrie és 1, i a partir d'ells el
-	
-		tercer membre i successius es calculen amb la suma dels dos anteriors.
-		
-		c. S’ha de tenir en compte que no es pot calcular la sèrie de Fibonacci per un número inferior a 2, i això
-		
-		s’ha de controlar.
+		/* 50 Hacer un programa que calcule N digitos de la serie Fibonacci
+		Ejemplo para N = 7, el resultado seria 1 1 2 3 5 8 13
+		a. Esta serie se calcula teniendo en cuenta que el primer y el segundo numero es 1 y a partir de ellos el 
+		tercero se calcula con la suma de los dos anteriores y asin sucesivamente		
+		b.Hay que tener en cuenta que no se puede calcular la serie con un valor inferior  a 2, hay que controlarlo
 		 */
 
 		Scanner entrada = new Scanner (System.in);
@@ -26,14 +18,17 @@ public class Ejercicio50 {
 		
 		limite = entrada.nextInt();
 		
+		//Escribimos los dos primeros numero y los restamos luego al hacer el bucle porque ya estan puesto
 		System.out.print("1 1");
 		
 		for (con = 0; con < limite-2; con++)
 		{
 			total= n1+n2;
+			
 			System.out.print(" "+total );
 			
 			n1=n2;
+			
 			n2=total;
 		}
 	}
