7f0c741e8dae246d6c9bd22a7d854e7e8e96c90c
Morgan <morgan.cordoba@hotmail.com>
2016-10-21 13:21:01 +0200

    Ejercicios 2 y 10 mejorados con try/catch

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -5,14 +5,21 @@ public class Ejercicio10 {
 	public static void main(String[] args) {
 		// 10. Convertir euros a pesetas
 		
-		double euro, resultado;
+		double euro= 0, resultado;
 		final double peseta = 166.39;
 
 		Scanner entrada = new Scanner (System.in);
 		
 		System.out.println("Introduce la cantidad de euros para convertirla a pesetas");
 		
-		euro = entrada.nextDouble();
+		try
+		{
+			euro = entrada.nextDouble();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Has introducido un valor con el que no se puede operar.");
+		}
 		
 		resultado = euro * peseta;
 		
--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -4,30 +4,28 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// 2. Introducir dos numeros y calcular la suma.
+		double num1 = 0, num2 = 0, resultado;
+		
+		Scanner entrada = new Scanner (System.in);
 		
 		try 
-		{
-			double num1, num2, resultado;
-			Scanner entrada = new Scanner (System.in);
-			
+		{			
 			System.out.println("Introduce el primer numero de la suma.");
 			
 			num1 = entrada.nextDouble();
 			
 			System.out.println("Introduce el segundo numero de la suma.");
 			
-			num2 = entrada.nextDouble();
-			
-			resultado = num1 + num2;
-			
-			System.out.println("El resultado de la suma es " + resultado);	 
+			num2 = entrada.nextDouble();			
 		}
-		
 		catch (Exception e)
 		{
 			System.out.println("Has introducido algun valor con el que no se puede operar");
-		}			
-
+		}	
+		
+		resultado = num1 + num2;
+			
+		System.out.println("El resultado de la suma es " + resultado);	 				
 	}
 
 }
